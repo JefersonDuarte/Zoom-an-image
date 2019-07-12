585059dea6cb2296a69c746353d1b13bdb0e0e82
Jeferson Duarte <jeferson.duarte@clickqi.com.br>
2019-07-12 13:06:16 -0300

    ident code

--- zoom-effect by  (Right way technology)/js/script.js
+++ zoom-effect by  (Right way technology)/js/script.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.my-zoom-1').WMZoom();
     $('.my-zoom-2').WMZoom({
-        config : {
-            inner : true
+        config: {
+            inner: true
         }
     });
 });
